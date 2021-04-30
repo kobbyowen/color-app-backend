aa3eceff50e6ff589283032b1050789a5c9260c1
Kobby Owen <Kobby@VOYAGER.LOCAL>
2021-04-30 12:47:46 +0000

    Added Procfile for heroku deployment

--- manage.py
+++ manage.py
@@ -11,8 +11,14 @@ database_commands = Blueprint("db", __name__)
 test_commands = Blueprint("test", __name__)
 
 
+def _create_dirs():
+    if not os.path.exists("data") : os.mkdir("data")
+    if not os.path.exists("tests/fixtures"): os.mkdir("tests/fixtures")
+    
+
 @database_commands.cli.command("create-all")
 def create_tables():
+    _create_dirs()
     create_all_tables()
 
 @database_commands.cli.command("drop-all")
@@ -22,11 +28,13 @@ def remove_tables():
 @test_commands.cli.command("all")
 @use_coverage
 def run_all_tests():
+    _create_dirs()
     tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @test_commands.cli.command("coverage-test")
 def run_coverage_tests():
+    _create_dirs()
     cmd = """coverage run --omit 'venv/*'  -m unittest discover "tests" && coverage html -d coverage_html"""
     os.system(cmd)
 
@@ -34,6 +42,7 @@ def run_coverage_tests():
 @click.argument("filename")
 @use_coverage
 def test_file(filename):
+    _create_dirs()
     tests = unittest.TestLoader().discover(
         "tests",
         pattern=f"*{filename}*")
